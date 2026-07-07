3b76855b1233ccdedf11132e8213862fefa815a3
IsmaelMorales2002 <104656736+IsmaelMorales2002@users.noreply.github.com>
2026-07-07 15:42:27 -0600

    Modelo Materia Actualizado - Prerequisito Agregado

--- src/main/java/com/code/code/model/Materia.java
+++ src/main/java/com/code/code/model/Materia.java
@@ -19,81 +19,92 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "Materia")
 public class Materia {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idMateria;
-    
+
     @Column(name = "nombre_materia", length = 100, nullable = false)
     private String nombreMateria;
-    
+
     @Column(name = "codigo_materia", length = 10, nullable = false)
     private String codigoMateria;
-    
+
     @Column(name = "uv_materia", nullable = false)
     private byte uvMateria;
-    
+
     @Column(name = "ciclo_materia", length = 20, nullable = false)
     private String cicloMateria;
-    
-    //promedio dependera de materia
+
+    @Column(name = "pre_requisito_materia", length = 25, nullable = false)
+    private String prerequisitoMateria;
+
+    // promedio dependera de materia
     @OneToMany(mappedBy = "materia", cascade = CascadeType.ALL)
     @JsonIgnoreProperties("materia")
     private List<Promedio> promedio;
-    
-    //programa dependera de materia
+
+    // programa dependera de materia
     @OneToMany(mappedBy = "materia", cascade = CascadeType.ALL)
     @JsonIgnoreProperties("materia")
     private List<Programa> programa;
-    
-    //materia depende de docente (llave foranea)
+
+    // materia depende de docente (llave foranea)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "idDocente", nullable = false)
     @JsonIgnoreProperties("Materia")
     private Docente docente;
-    
-    //materia depende de carrera (llave foranea)
+
+    // materia depende de carrera (llave foranea)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "idCarrera", nullable = false)
     @JsonIgnoreProperties("Materia")
     private Carrera carrera;
-    
+
+    public String getPrerequisitoMateria() {
+        return prerequisitoMateria;
+    }
+
+    public void setPrerequisitoMateria(String prerequisitoMateria) {
+        this.prerequisitoMateria = prerequisitoMateria;
+    }
+
     public Long getIdMateria() {
         return idMateria;
     }
-    
+
     public void setIdMateria(Long idMateria) {
         this.idMateria = idMateria;
     }
-    
+
     public String getNombreMateria() {
         return nombreMateria;
     }
-    
+
     public void setNombreMateria(String nombreMateria) {
         this.nombreMateria = nombreMateria;
     }
-    
+
     public String getCodigoMateria() {
         return codigoMateria;
     }
-    
+
     public void setCodigoMateria(String codigoMateria) {
         this.codigoMateria = codigoMateria;
     }
-    
+
     public byte getUvMateria() {
         return uvMateria;
     }
-    
+
     public void setUvMateria(byte uvMateria) {
         this.uvMateria = uvMateria;
     }
-    
+
     public String getCicloMateria() {
         return cicloMateria;
     }
-    
+
     public void setCicloMateria(String cicloMateria) {
         this.cicloMateria = cicloMateria;
     }
