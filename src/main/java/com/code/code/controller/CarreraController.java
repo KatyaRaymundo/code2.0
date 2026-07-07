9256e20ce56b665a317a3e546cfa638f06fe30d2
IsmaelMorales2002 <104656736+IsmaelMorales2002@users.noreply.github.com>
2026-07-07 08:51:17 -0600

    Actualizacion De CarreraController

--- src/main/java/com/code/code/controller/CarreraController.java
+++ src/main/java/com/code/code/controller/CarreraController.java
@@ -1,28 +1,27 @@
 package com.code.code.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.code.code.Repository.CarreraRepository;
 import com.code.code.model.Carrera;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
-
 @RestController
 @RequestMapping("/carreras")
+@RequiredArgsConstructor
 public class CarreraController {
-    
-    @Autowired
-    private CarreraRepository carreraRepository;
+
+    private final CarreraRepository carreraRepository;
 
     @GetMapping("/verCarrera")
     public List<Carrera> obtenerCarrera() {
@@ -31,17 +30,17 @@ public class CarreraController {
 
     @PostMapping("/crearCarrera")
     public Carrera crearCarrera(@RequestBody Carrera nuevaCarrera) {
-        
+
         return carreraRepository.save(nuevaCarrera);
     }
-    
+
     @PutMapping("/editarCarrera/{id}")
     public Carrera editarCarrera(@PathVariable Long id, @RequestBody Carrera editarCarrera) {
 
         return carreraRepository.findById(id).map(carreraExistente -> {
             carreraExistente.setNombreCarrera(editarCarrera.getNombreCarrera());
             return carreraRepository.save(carreraExistente);
-        }).orElseThrow(()->new RuntimeException("Error, Carrera no Encontrada"));
+        }).orElseThrow(() -> new RuntimeException("Error, Carrera no Encontrada"));
     }
-    
+
 }
