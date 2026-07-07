adc3abfc08caad8dcc0d57f10522b4e53b112402
IsmaelMorales2002 <104656736+IsmaelMorales2002@users.noreply.github.com>
2026-07-07 16:04:14 -0600

    Horario - Inscribir Repository Agregado

---
+++ src/main/java/com/code/code/Repository/HorarioRepository.java
@@ -0,0 +1,9 @@
+package com.code.code.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.code.code.model.Horario;
+
+public interface HorarioRepository extends JpaRepository<Horario, Long> {
+
+}
---
+++ src/main/java/com/code/code/Repository/InscribirRepository.java
@@ -0,0 +1,9 @@
+package com.code.code.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.code.code.model.Inscribir;
+
+public interface InscribirRepository extends JpaRepository<Inscribir, Long> {
+
+}
