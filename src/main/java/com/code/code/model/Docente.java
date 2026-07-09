469f3492faa5a96e1bafbafe43fa0967197bc9e3
IsmaelMorales2002 <104656736+IsmaelMorales2002@users.noreply.github.com>
2026-07-09 16:00:35 -0600

    Modelo Estudiante & Docente Modificado - Clave 255

--- src/main/java/com/code/code/model/Docente.java
+++ src/main/java/com/code/code/model/Docente.java
@@ -1,6 +1,5 @@
 package com.code.code.model;
 
-
 import java.util.Date;
 import java.util.List;
 
@@ -18,132 +17,131 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "Docentes")
 public class Docente {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idDocente;
-    
+
     @Column(name = "nombre_docente", length = 100, nullable = false)
     private String nombreDocente;
-        
+
     @Column(name = "apellido_docente", length = 100, nullable = false)
     private String apellidoDocente;
-    
+
     @Column(name = "fecha_nacimiento_docente", nullable = false)
     private Date fechaNacimientoDocente;
-    
+
     @Column(name = "genero_docente", nullable = false)
     private char generoDocente;
-    
+
     @Column(name = "correo_docente", length = 150, nullable = false)
     private String correoDocente;
-    
+
     @Column(name = "telefono_docente", length = 15, nullable = false)
     private String telefonoDocente;
-    
+
     @Column(name = "carnet_docente", length = 10, nullable = false)
     private String carnetDocente;
-    
+
     @Column(name = "titulo_docente", length = 150, nullable = false)
     private String tituloDocente;
-    
-    @Column(name = "clave_docente", length = 10, nullable = false)
+
+    @Column(name = "clave_docente", length = 255, nullable = false)
     private String claveDocente;
-    
+
     @Column(name = "imagen_docente", length = 250, nullable = false)
     private String imagenDocente;
-    
+
     @OneToMany(mappedBy = "docente", cascade = CascadeType.ALL)
     @JsonIgnoreProperties("docente")
     private List<Materia> materias;
-    
-    
+
     public Long getIdDocente() {
         return idDocente;
     }
-    
+
     public void setIdDocente(Long idDocente) {
         this.idDocente = idDocente;
     }
-    
+
     public String getNombreDocente() {
         return nombreDocente;
     }
-    
+
     public void setNombreDocente(String nombreDocente) {
         this.nombreDocente = nombreDocente;
     }
-    
+
     public String getApellidoDocente() {
         return apellidoDocente;
     }
-    
+
     public void setApellidoDocente(String apellidoDocente) {
         this.apellidoDocente = apellidoDocente;
     }
-    
+
     public Date getFechaNacimientoDocente() {
         return fechaNacimientoDocente;
     }
-    
+
     public void setFechaNacimientoDocente(Date fechaNacimientoDocente) {
         this.fechaNacimientoDocente = fechaNacimientoDocente;
     }
-    
+
     public char getGeneroDocente() {
         return generoDocente;
     }
-    
+
     public void setGeneroDocente(char generoDocente) {
         this.generoDocente = generoDocente;
     }
-    
+
     public String getCorreoDocente() {
         return correoDocente;
     }
-    
+
     public void setCorreoDocente(String correoDocente) {
         this.correoDocente = correoDocente;
     }
-    
+
     public String getTelefonoDocente() {
-            return telefonoDocente;
+        return telefonoDocente;
     }
 
     public void setTelefonoDocente(String telefonoDocente) {
-            this.telefonoDocente = telefonoDocente;
+        this.telefonoDocente = telefonoDocente;
     }
-    
+
     public String getCarnetDocente() {
         return carnetDocente;
     }
-    
+
     public void setCarnetDocente(String carnetDocente) {
         this.carnetDocente = carnetDocente;
     }
-    
+
     public String getTituloDocente() {
         return tituloDocente;
     }
-    
+
     public void setTituloDocente(String tituloDocente) {
         this.tituloDocente = tituloDocente;
     }
-    
+
     public String getClaveDocente() {
         return claveDocente;
     }
-    
+
     public void setClaveDocente(String claveDocente) {
         this.claveDocente = claveDocente;
     }
-    
+
     public String getImagenDocente() {
         return imagenDocente;
     }
-    
+
     public void setImagenDocente(String imagenDocente) {
         this.imagenDocente = imagenDocente;
     }
-        
+
 }
