c5b49cae9f7a2428c62ce6982894e66cf47c614b
IsmaelMorales2002 <104656736+IsmaelMorales2002@users.noreply.github.com>
2026-07-09 18:29:21 -0600

    Modelo Usuario Agregado & Herencia hacia Estudiante - Docente

---
+++ src/main/java/com/code/code/model/Usuario.java
@@ -0,0 +1,170 @@
+package com.code.code.model;
+
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Usuarios")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idUsuario;
+
+    @Column(name = "nombre_usuario", length = 100, nullable = false)
+    private String nombreUsuario;
+
+    @Column(name = "apellido_usuario", length = 100, nullable = false)
+    private String apellidoUsuario;
+
+    @Column(name = "fecha_nacimiento_usuario", nullable = false)
+    private Date fechaNacimientoUsuario;
+
+    @Column(name = "genero_usuario", nullable = false)
+    private char generoUsuario;
+
+    @Column(name = "correo_usuario", length = 150, nullable = false)
+    private String correoUsuario;
+
+    @Column(name = "telefono_usuario", length = 15, nullable = false)
+    private String telefonoUsuario;
+
+    @Column(name = "carnet_usuario", length = 10, nullable = false)
+    private String carnetUsuario;
+
+    @Column(name = "clave_usuario", length = 255, nullable = false)
+    private String claveUsuario;
+
+    @Column(name = "imagen_usuario", length = 250, nullable = false)
+    private String imagenUsuario;
+
+    @Column(name = "tipo_usuario", length = 15, nullable = false)
+    private String tipoUsuario;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("usuario")
+    private List<Docente> docente;
+
+    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
+    @JsonIgnoreProperties("usuario")
+    private List<Estudiante> estudiante;
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getApellidoUsuario() {
+        return apellidoUsuario;
+    }
+
+    public void setApellidoUsuario(String apellidoUsuario) {
+        this.apellidoUsuario = apellidoUsuario;
+    }
+
+    public Date getFechaNacimientoUsuario() {
+        return fechaNacimientoUsuario;
+    }
+
+    public void setFechaNacimientoUsuario(Date fechaNacimientoUsuario) {
+        this.fechaNacimientoUsuario = fechaNacimientoUsuario;
+    }
+
+    public char getGeneroUsuario() {
+        return generoUsuario;
+    }
+
+    public void setGeneroUsuario(char generoUsuario) {
+        this.generoUsuario = generoUsuario;
+    }
+
+    public String getCorreoUsuario() {
+        return correoUsuario;
+    }
+
+    public void setCorreoUsuario(String correoUsuario) {
+        this.correoUsuario = correoUsuario;
+    }
+
+    public String getTelefonoUsuario() {
+        return telefonoUsuario;
+    }
+
+    public void setTelefonoUsuario(String telefonoUsuario) {
+        this.telefonoUsuario = telefonoUsuario;
+    }
+
+    public String getCarnetUsuario() {
+        return carnetUsuario;
+    }
+
+    public void setCarnetUsuario(String carnetUsuario) {
+        this.carnetUsuario = carnetUsuario;
+    }
+
+    public String getClaveUsuario() {
+        return claveUsuario;
+    }
+
+    public void setClaveUsuario(String claveUsuario) {
+        this.claveUsuario = claveUsuario;
+    }
+
+    public String getImagenUsuario() {
+        return imagenUsuario;
+    }
+
+    public void setImagenUsuario(String imagenUsuario) {
+        this.imagenUsuario = imagenUsuario;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public List<Docente> getDocente() {
+        return docente;
+    }
+
+    public void setDocente(List<Docente> docente) {
+        this.docente = docente;
+    }
+
+    public List<Estudiante> getEstudiante() {
+        return estudiante;
+    }
+
+    public void setEstudiante(List<Estudiante> estudiante) {
+        this.estudiante = estudiante;
+    }
+
+}
