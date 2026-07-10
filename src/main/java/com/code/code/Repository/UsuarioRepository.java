d44986469c30489ba117c5a01a19a6e1016d75fe
IsmaelMorales2002 <104656736+IsmaelMorales2002@users.noreply.github.com>
2026-07-09 19:20:20 -0600

    UsuarioRepository Creado y Agregado ByCorreoUsuario

--- src/main/java/com/code/code/Repository/UsuarioRepository.java
+++ src/main/java/com/code/code/Repository/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package com.code.code.Repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.code.code.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+    Optional<Usuario> findByCorreoUsuario(String correoUsuario);
 }
